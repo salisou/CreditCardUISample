aa6b90646b19aa72cafd7bdc038e3dc9a5a3e731
Moussa <salisoumoussa1@gmail.com>
2023-03-09 19:27:12 +0100

    Aggiungere i file di progetto.

---
+++ CreditCardUISample/CreditCardUISample/Views/CreditCardPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CreditCardUISample.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CreditCardUISample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    [DesignTimeVisible(true)]
+    public partial class CreditCardPage : ContentPage
+    {
+        public CreditCardPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new CreditCardPageViewModel();
+        }
+    }
+}
---
+++ CreditCardUISample/CreditCardUISample/Views/CustomNavigationPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CreditCardUISample.Views
+{
+    public partial class CustomNavigationPage : NavigationPage
+    {
+        public CustomNavigationPage()
+        {
+            InitializeComponent();
+        }
+
+        public CustomNavigationPage(Page root): base(root)
+        {
+            InitializeComponent();
+        }
+    }
+}
